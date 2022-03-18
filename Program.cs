7fba64ddc04c3acc05d70a7043a436df72fad040
VANDO MONTEIRO <vando.monteiro@etec.sp.gov.br>
2022-03-18 11:25:36 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1900_contar_1_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                        
+            Console.WriteLine("Contar de 1 até 10");
+
+            Console.WriteLine();
+
+            for (int contador = 1; contador <= 9; contador++)
+            {
+
+                Console.Write(contador + ", ");
+
+            }
+
+            Console.WriteLine("10.");
+            Console.ReadLine();
+
+        }
+    }
+}
